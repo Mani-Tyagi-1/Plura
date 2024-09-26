11b6a7fdabec47d9a1790f2bcb7ff7e1ef7f3372
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2024-09-26 19:29:10 +0530

    Auth Added

---
+++ src/app/(main)/agency/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+import { Sign } from "crypto";
+import React from "react";
+
+const page = () => {
+  return <SignIn />;
+};
+
+export default page;
---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { authMiddleware, clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+
+export default authMiddleware({
+    publicRoutes: ["/site", "/api/uploadthing"],
+})
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
